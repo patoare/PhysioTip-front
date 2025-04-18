bc3655899fafe0a3225ddaa30eae9aff39c49391
patricio <patoare@hotmail.com>
2025-04-18 11:10:18 +0200

    cmbios footer

--- PhysioTip/src/components/Footer.jsx
+++ PhysioTip/src/components/Footer.jsx
@@ -6,13 +6,20 @@ const Footer = () => {
     return (
         <>
         <div className="footer-container">
+            <div className='footer-column1'>
         <div className="logo">
             <Lottie animationData={Logo} loop={true} />
-        </div>
-        <div className="tituloFooter">
+            </div> 
         <h1>PhysioTip</h1>
-        
         </div>
+        <div className='footer-column2'>
+            <h6>Designed and Creatd By</h6>
+            <a href='#'>patoare</a>
+            </div>
+            <div className='footer-column3'>
+                <a href='#'>Instagram</a>
+                <a href='#'>Linkedin</a>
+            </div>
     </div>
     
     </>
